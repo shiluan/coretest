46f1d2121544b93ee28933f24d3cc5cc7372ff35
shiluan <shi.luan@hotmail.com>
2021-01-27 17:32:43 -0500

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace coretest
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World! Kevin");
+                    await context.Response.WriteAsync("Hello World! Kevin 2");
                 });
             });
         }
